a63da88c0c28a2ba2d12a2973b8dbf0d4dfd37bf
Rio6 <rio6.liu@gmail.com>
2020-02-01 23:34:54 -0800

    Make fallocate work on pi

--- filetest.py
+++ filetest.py
@@ -8,7 +8,7 @@ import hashlib
 from logzero import logger
 
 # constants
-MAX_SIZE = 1024*1024*1024*2 # will allocate 2 GB at max on file system
+MAX_SIZE = 2147483647 # will allocate 2 GB at max on file system. it is also the max int32
 RESERVE_SIZE = 1024*1024*100 # reserve at least 100 MB after allocation
 FILE_NAME = "experiment_use_only_do_not_upload"
 BLOCK_SIZE = 1024*1024
@@ -19,15 +19,14 @@ libc.fallocate.restype = ctypes.c_int
 libc.fallocate.argtypes = [c.c_int, c.c_int, c.c_int32, c.c_int32]
 
 def fallocate(fd, mode, offset, length):
-    # https://gist.github.com/NicolasT/1194957
-    res = libc.fallocate(fd.fileno(), mode, offset, length)
+    res = libc.fallocate(fd, mode, offset, length)
     if res != 0:
-        raise IOError(res, 'Failed to fallocate file')
+        raise IOError(c.get_errno(), 'Failed to fallocate file')
 
 class FileTest:
     def __init__(self, pwd):
         self.path = pwd + '/' + FILE_NAME
-        self.fd = None
+        self.file = None
 
         # find out available storage space
         free = shutil.disk_usage(pwd).free - RESERVE_SIZE
@@ -40,19 +39,24 @@ class FileTest:
     def __enter__(self):
         if self.size > 0:
             logger.info("Allocating %d bytes on filesystem", self.size)
+            fd = -1
             try:
-                with open(self.path, 'a') as fd:
-                    fallocate(fd, 0, 0, self.size)
-                self.fd = open(self.path, 'rb', 0, flags=os.O_DIRECT)
+                fd = os.open(self.path, os.O_RDWR | os.O_CREAT)
+                fallocate(fd, 0, 0, self.size)
+                self.file = open(self.path, 'rb', 0)
+                logger.info("File allocated")
                 self.update_hash()
-            except:
-                logger.error("Failed to allocate")
+            except (IOError, OSError) as e:
+                logger.error(str(e))
+            finally:
+                if fd != -1:
+                    os.close(fd)
         return self
 
     def __exit__(self, *args):
         logger.info("Deleting experiment file")
-        if self.fd: self.fd.close()
-        self.fd = None
+        if self.file: self.file.close()
+        self.file = None
         try:
             os.remove(self.path)
         except OSError:
@@ -68,7 +72,7 @@ class FileTest:
         self.hash = md5.hexdigest()
 
     def test(self):
-        if not self.fd: return {'file_changed': False, 'file_size': 0}
+        if not self.file: return {'file_changed': False, 'file_size': 0}
 
         hash = self.hash
         update_hash()
