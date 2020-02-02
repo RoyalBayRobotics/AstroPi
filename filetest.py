72e71852158ad927e1a51a27978be9cb02a95388
Rio6 <rio6.liu@gmail.com>
2020-02-02 13:24:54 -0800

    Use O_DIRECT to read file when doing checksum

--- filetest.py
+++ filetest.py
@@ -2,6 +2,7 @@ import ctypes as c
 import ctypes.util
 
 import os
+import mmap
 import shutil
 from zlib import adler32
 
@@ -11,7 +12,7 @@ from logzero import logger
 MAX_SIZE = 2147483647 # will allocate 2 GB at max on file system. it is also the max int32
 RESERVE_SIZE = 1024*1024*100 # reserve at least 100 MB after allocation
 FILE_NAME = "experiment_use_only_do_not_upload"
-BLOCK_SIZE = 1024*1024*10
+READ_SIZE = 1024*1024
 
 # prepare fallocate function
 libc = c.CDLL(ctypes.util.find_library('c'), use_errno=True)
@@ -19,14 +20,14 @@ libc.fallocate.restype = ctypes.c_int
 libc.fallocate.argtypes = [c.c_int, c.c_int, c.c_int32, c.c_int32]
 
 def fallocate(fd, mode, offset, length):
-    res = libc.fallocate(fd.fileno(), mode, offset, length)
+    res = libc.fallocate(fd, mode, offset, length)
     if res != 0:
         raise IOError(c.get_errno(), 'Failed to fallocate file')
 
 class FileTest:
     def __init__(self, pwd):
         self.path = pwd + '/' + FILE_NAME
-        self.file = None
+        self.fd = -1
 
         # find out available storage space
         free = shutil.disk_usage(pwd).free - RESERVE_SIZE
@@ -40,30 +41,31 @@ class FileTest:
         if self.size > 0:
             logger.info("Allocating %d bytes on filesystem", self.size)
             try:
-                self.file = open(self.path, 'w+b', 0)
-                fallocate(self.file, 0, 0, self.size)
+                self.fd = os.open(self.path, os.O_RDWR | os.O_CREAT | os.O_TRUNC | os.O_DIRECT)
+                fallocate(self.fd, 0, 0, self.size)
                 logger.info("File allocated")
-                self.update_hash()
+                #self.update_hash()
             except (IOError, OSError) as e:
                 logger.error(str(e))
         return self
 
     def __exit__(self, *args):
         logger.info("Deleting experiment file")
-        if self.file: self.file.close()
-        self.file = None
+        if self.fd != -1: os.close(self.fd)
+        self.fd = -1
         try:
             os.remove(self.path)
         except OSError:
             pass
 
-    def update_hash(self, block_size=BLOCK_SIZE):
-        self.hash = 0xFFFFFF
-        self.file.seek(0)
-        while True:
-            data = self.file.read(block_size)
+    def update_hash(self, read_size=READ_SIZE):
+        self.hash = 1
+        pos = 0
+        while pos < self.size:
+            data = self._direct_read(pos, min(read_size, self.size-pos))
             if not data: break
             self.hash = adler32(data, self.hash)
+            pos += len(data)
 
     def test(self):
         if not self.file: return {'file_changed': False, 'file_size': 0}
@@ -74,3 +76,7 @@ class FileTest:
             'file_changed': hash != update_hash,
             'file_size': self.size,
         }
+
+    def _direct_read(self, pos, size):
+        with mmap.mmap(self.fd, size, offset=pos, access=mmap.ACCESS_READ) as data:
+            return data.read(size)
