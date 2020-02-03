450f42f0925f1e67e0682a2a6f9412d22951eb02
Rio6 <rio6.liu@gmail.com>
2020-02-02 19:45:03 -0800

    Use generator to do multitasking

--- bittest.py
+++ bittest.py
@@ -10,12 +10,10 @@ import numpy as np
 from logzero import logger
 
 # constants
-MAX_SIZE=0
-#MAX_SIZE = 2147483647 # will allocate 2 GB at max on file system. it is also the max int32
+MAX_SIZE = 2147483647 # will allocate 2 GB at max on file system. it is also the max int32
 RESERVE_SIZE = 1024*1024*100 # reserve at least 100 MB after allocation
 MEM_SIZE = 1024*1024*500 # Will allocate 500 MB of ram
 FILE_NAME = "experiment_use_only_do_not_upload"
-READ_SIZE = 1024*4
 
 # prepare fallocate function
 libc = c.CDLL(ctypes.util.find_library('c'), use_errno=True)
@@ -27,10 +25,13 @@ def fallocate(fd, mode, offset, length):
     if res != 0:
         raise IOError(c.get_errno(), 'Failed to fallocate file')
 
+# uses generator to implement multitask
 class FileTest:
-    def __init__(self, pwd):
+    def __init__(self, pwd, read_size=1024*1024):
         self.path = pwd + '/' + FILE_NAME
         self.fd = -1
+        self.hash = 1
+        self.read_size = read_size
 
         # find out available storage space
         free = shutil.disk_usage(pwd).free - RESERVE_SIZE
@@ -47,7 +48,6 @@ class FileTest:
                 self.fd = os.open(self.path, os.O_RDWR | os.O_CREAT | os.O_TRUNC | os.O_DIRECT)
                 fallocate(self.fd, 0, 0, self.size)
                 logger.info("File allocated")
-                self.update_hash()
             except (IOError, OSError) as e:
                 logger.error(str(e))
         return self
@@ -61,22 +61,28 @@ class FileTest:
         except OSError:
             pass
 
-    def update_hash(self, read_size=READ_SIZE):
+    def update_hash(self):
+        if self.fd == -1: return
+
         self.hash = 1
         pos = 0
         while pos < self.size:
-            data = self._direct_read(pos, min(read_size, self.size-pos))
+            data = self._direct_read(pos, min(self.read_size, self.size-pos))
             if not data: break
             self.hash = adler32(data, self.hash)
             pos += len(data)
+            yield pos / self.size
 
     def test(self):
-        if self.fd == -1: return {'file_changed': False, 'file_size': 0}
+        if self.fd == -1: yield {'file_changed': False, 'file_size': 0}
 
         hash = self.hash
-        update_hash()
-        return {
-            'file_changed': hash != update_hash,
+        for prog in self.update_hash():
+            # wait for it to calculating file checksum
+            yield prog
+
+        yield {
+            'file_changed': hash != self.hash,
             'file_size': self.size,
         }
 
@@ -85,22 +91,30 @@ class FileTest:
             return data.read(size)
 
 class MemoryTest:
-    def __init__(self):
+    def __init__(self, batch_size=MEM_SIZE):
+        self.batch_size = batch_size
+
         logger.info("Allocating %d bytes of memory", MEM_SIZE)
         # Using both zeros and ones to see if different bits gives different results
         self.array = np.full(MEM_SIZE, 0xFF, dtype=np.uint8)
         self.array[:MEM_SIZE//2] = 0x00
         logger.info("Memory allocated")
-        self._update_hash()
 
-    def _update_hash(self):
-        self.hash_zero = adler32(self.array.data[:MEM_SIZE//2])
-        self.hash_one = adler32(self.array.data[MEM_SIZE//2:])
+    def update_hash(self):
+        self.hash_zero = self.hash_one = 1
+        for i in range(0, self.array.size, self.batch_size):
+            self.hash_zero = adler32(self.array.data[i:i+self.batch_size], self.hash_zero)
+            self.hash_one = adler32(self.array.data[i:i+self.batch_size], self.hash_one)
+            yield (i+self.batch_size) / self.array.size
 
     def test(self):
         hash_zero, hash_one = self.hash_zero, self.hash_one
-        self._update_hash()
-        return {
+
+        for prog in self.update_hash():
+            # wait for it
+            yield prog
+
+        yield {
             'memory_zero_changed': hash_zero != self.hash_zero,
             'memory_one_changed': hash_one != self.hash_one,
         }
--- main.py
+++ main.py
@@ -11,94 +11,159 @@ from logzero import logger
 from sensors import Sensors, Camera
 from bittest import MemoryTest, FileTest
 
+# global variables
+num_datas = 3
+data_empty = [True] * num_datas
+data_loggers = [None] * num_datas
+
 # time keeping variables
-MAX_RUN_TIME = 10 # seconds TODO remember to change it to 3 hours
+MAX_RUN_TIME = 60 # seconds TODO remember to change it to 3 hours
 start_time = time.time()
 last_run_time = 0 # used when the first run got interrupted
 
 # file paths
 path = os.path.dirname(os.path.realpath(__file__))
-data_file = path + '/data01.csv'
-log_file = path + '/data02.log'
-img_file = path + '/image{}.jpg'
+img_file = path + '/image{}.jpg' # gets formatted when saving
+data_files = [path + '/data{:02d}.csv'.format(n+1) for n in range(num_datas)]
+log_file = path + '/data{:02d}.log'.format(num_datas+1)
+
+def log_data(data, n):
+    global data_empty
+
+    # write CSV keys to file if it's empty
+    if data_empty[n]:
+        data_loggers[n].info(','.join(data.keys()))
+        data_empty[n] = False
+
+    # write datas to file
+    data_loggers[n].info(','.join(str(v) for v in data.values()))
+
+def gen_task_sensors():
+    camera = Camera(img_file, min_interval=60)
+    sensors = Sensors()
+
+    last_time = 0
+    while True:
+        now = time.time()
+
+        # limit execution rate to 1/sec
+        if now - last_time < 1:
+            yield
+
+        # record results
+        data = {'time': time.time()}
+        data.update(sensors.get_data())
+        data.update(camera.update())
+
+        log_data(data, 0)
+
+        last_time = now
+        yield
+
+def gen_task_memory_test():
+    memtest = MemoryTest(batch_size=1024*1024*50)
+
+    # initial checksum for memory is fast enough to do it all at once
+    for rst in memtest.update_hash():
+        logger.debug("Memory checksum progress: %d%%", rst * 100)
+
+    yield
+
+    while True:
+        for rst in memtest.test():
+            if type(rst) is dict: break
+            logger.debug("Memory test progress: %d%%", rst * 100)
+            yield
+
+        data = {'time': time.time()}
+        data.update(rst)
+        log_data(data, 1)
+
+def gen_task_file_test():
+    with FileTest(path) as filetest:
+
+        # calculate initial checksum
+        for rst in filetest.update_hash():
+            logger.debug("File checksum progress: %d%%", rst * 100)
+            yield
+
+        while True:
+            for rst in filetest.test():
+                if type(rst) is dict: break
+                logger.debug("File test progress: %d%%", rst * 100)
+                yield
+
+            data = {'time': time.time()}
+            data.update(rst)
+            log_data(data, 2)
 
 def main():
-    global last_run_time
+    global last_run_time, data_loggers, data_empty
 
-    # save program output
+    # setup general logging
     logzero.logfile(log_file)
 
     logger.info("Initializing")
 
-    # logger for logging sensor datas
-    file_logger = logzero.setup_logger(logfile=data_file, disableStderrLogger=True,
-            formatter=logging.Formatter('%(message)s', datefmt='%s'))
+    # logger for datas
+    formatter=logging.Formatter('%(message)s', datefmt='%s')
+    for i in range(num_datas):
+        data_loggers[i] = logzero.setup_logger(name=str(i), logfile=data_files[i], disableStderrLogger=True, formatter=formatter)
+        data_empty[i] = os.stat(data_files[i]).st_size == 0
 
-    is_empty_file = os.stat(data_file).st_size == 0
+    # figure out how much runtime there is left using one of the data files
+    if data_empty[0]: # fresh start, record start time
+        data_loggers[0].info(time.time())
 
-    # figure out how much runtime there is left
-    if is_empty_file: # first start, record initial start time
-        file_logger.info(time.time())
-
-    else: # not first start, read the previous runtime
-        with open(data_file, 'rb') as file:
+    else: # not fresh start, read the previous runtime
+        with open(data_files[0], 'rb') as file:
             # start time
-            start = file.readline().decode().split(',')[0]
+            start = file.readline().decode()
 
             # seek backwards until EOL
-            if start.isdigit():
-                try:
-                    file.seek(-2, os.SEEK_END)
-                    while file.read(1) != b"\n":
-                        file.seek(-2, os.SEEK_CUR)
+            try:
+                file.seek(-2, os.SEEK_END)
+                while file.read(1) != b"\n":
+                    file.seek(-2, os.SEEK_CUR)
 
-                    end = file.readline().decode().split(',')[0]
+                end = file.readline().decode().split(',')[0]
 
-                    if end.isdigit():
-                        # save the run time of last execution if there was one
-                        last_run_time = int(end) - int(start)
+                # save the run time of last execution if there was one
+                last_run_time = float(end) - float(start)
 
-                except OSError:
-                    pass
+            except (OSError, ValueError):
+                logger.warning("Cannot obtain last run time")
+                pass
 
-    # init camera and sensors
-    camera = Camera(img_file, min_interval=1) # TODO remember to change it to 1 minute
-    sensors = Sensors()
-    memTest = MemoryTest()
-    fileTest_ = FileTest(path)
+    if last_run_time > 0:
+        logger.info("Already run for: %fs", last_run_time)
 
-    with fileTest_ as fileTest:
-        logger.info("Running")
+    # create tasks
+    tasks = [ # (task, runs per iteration)
+        (gen_task_sensors(), 1),
+        (gen_task_memory_test(), 1),
+        (gen_task_file_test(), 50)
+    ]
 
-        last_time = 0
-        while True:
-            now = time.time()
+    logger.info("Running")
 
-            # check runtime
-            if now - start_time + last_run_time > MAX_RUN_TIME: break # TODO account for loop execution time
+    while True:
+        now = time.time()
 
-            # limit execution rate to 1/sec
-            if now - last_time < 1:
-                time.sleep(1 - (now-last_time))
-                continue
-            last_time = now
+        # check runtime
+        if now - start_time + last_run_time > MAX_RUN_TIME: # TODO account for loop execution time
+            break
 
-            # get datas
-            data = {'time': time.time()}
-            data.update(sensors.get_data())
-            data.update(camera.update())
-            data.update(memTest.test())
-            data.update(fileTest.test())
+        # execute the tasks
+        for task in tasks:
+            try:
+                for i in range(task[1]):
+                    next(task[0])
+            except StopIteration:
+                logger.warning("%s ended early", task.__name__)
+                tasks.remove(task)
 
-            # write CSV keys to file when it's empty
-            if is_empty_file:
-                file_logger.info(','.join(data.keys()))
-                is_empty_file = False
-
-            # write datas to file
-            file_logger.info(','.join(str(v) for v in data.values()))
-
-            logger.info("Time taken in loop: %fs", time.time() - now)
+        logger.debug("Time taken in loop: %fs", time.time() - now)
 
     logger.info("Ending program")
 
